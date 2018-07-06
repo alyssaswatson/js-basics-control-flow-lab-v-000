42b2a21b9086402cdbca152558f28dd05937018f
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-07-06 02:27:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,12 @@ function ternaryCheckCity(city) {
 }
 
 function switchOnCharmFromTip(tip) {
+  switch (tip) {
+  case 'generous':
+    return 'Thank you so much.';
+  case 'not as generous':
+    return 'Thank you.';
+  default:
+    return 'Bye.';
 
 }
