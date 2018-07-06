8197454f5be06d1c6cd28e233502421ba29be540
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-07-06 02:22:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someValue) {
-  //this is where we can use conditionals given our argument, someValue
-  //don't forget to return whatever the result is!
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (1999 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
